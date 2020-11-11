5d811ec982cabee5657b202f32aad2809c84c9ff
lucordes <cordes135@gmail.com>
2020-11-11 22:42:01 +0100

    Amongus

--- keyboards/handwired/transplanck/keymaps/default/keymap.c
+++ keyboards/handwired/transplanck/keymaps/default/keymap.c
@@ -11,6 +11,7 @@ extern keymap_config_t keymap_config;
 
 enum keyboard_layers {
   _QWERTY = 0, // Base Layer
+  _AMONGUS,
   _UMLAUT,
   _LOWER,
   _RAISE,
@@ -90,7 +91,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 
-
+[_AMONGUS] = LAYOUT_ortho_4x12(
+  KC_ESC,  KC_Q,       KC_W,       KC_E,       KC_R,   TG(_AMONGUS),   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
+  KC_TAB, KC_A,       KC_S,       KC_D,       KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_ESC, KC_Z,       KC_X,       KC_C,       KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
+  KC_LCTL, KC_LGUI,    KC_RALT,    KC_LALT,    KC_SPC,  KC_SPC, KC_SPC, MO(_RAISE),  KC_RCTL, KC_RGUI, KC_LALT,   MO(_ARROW)
+),
 
 
 /* Lower
@@ -105,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12(
-  RESET, _______,  KC_WBAK,  KC_UP,  KC_WFWD,  _______,  _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,    KC_LPRN,  KC_RPRN,
+  RESET, _______,  KC_WBAK,  KC_UP,  KC_WFWD,  TG(_AMONGUS),  _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,    KC_LPRN,  KC_RPRN,
   ALTTAB, TABCYCLE,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_ENT , _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,    KC_LCBR,  KC_RCBR,
   _______, _______, _______,  _______,  KC_BSPC,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,  _______,      KC_LBRC,  KC_RBRC,
   _______, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  KC_PWR
@@ -237,8 +243,10 @@ uint8_t cur_dance(qk_tap_dance_state_t *state) {
 
 
     }
-    if (state->count > 1){return DOUBLE_SINGLE_TAP;}
-    else{return 8;}
+    //if (state->count ==0){return SINGLE_TAP;}
+   if (state->count > 1){return DOUBLE_SINGLE_TAP;}
+  else{return 8;}
+    //else{return 8;}
     /*
      else if (state->count == 2) {
          if (state->interrupted) return DOUBLE_SINGLE_TAP;}
